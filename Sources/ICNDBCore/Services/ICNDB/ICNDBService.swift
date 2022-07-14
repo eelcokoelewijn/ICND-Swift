7ab92e5f74c24d910c72c7d7b22d9d07f0cb43fb
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2022-07-14 19:49:26 +0200

    chore: Cleanup

--- Sources/ICNDBCore/Services/ICNDB/ICNDBService.swift
+++ Sources/ICNDBCore/Services/ICNDB/ICNDBService.swift
@@ -32,16 +32,7 @@ public class MixInICNDBService: ICNDBService {
                               substituteLastname lastName: String? = nil,
                               numberOfJokes number: Int = 1,
                               completion: @escaping ([String])  throws -> Void) {
-        let url = networkService.baseURL.appendingPathComponent("jokes/random/\(number)")
-        var params: [String: String] = [:]
-        if let firstName = firstName, let lastName = lastName {
-            params["firstName"] = firstName
-            params["lastName"] = lastName
-        }
-        let request = RequestBuilder(url: url).parameters(params).build()
-        let resource = Resource<Jokes>(request: request, parseResponse: { data in
-            return try? JSONDecoder().decode(Jokes.self, from: data)
-        })
+        let resource = getRandomJokeResource(firstName: firstName, lastName: lastName, numberOfJokes: number)
         let semaphore = DispatchSemaphore(value: 0)
         networkService.load(resource: resource) { result in
             defer {
@@ -59,26 +50,13 @@ public class MixInICNDBService: ICNDBService {
         substituteLastname lastName: String? = nil, 
         numberOfJokes number: Int = 1
     ) async throws -> [String] {
-        let url = networkService.baseURL.appendingPathComponent("jokes/random/\(number)")
-        var params: [String: String] = [:]
-        if let firstName = firstName, let lastName = lastName {
-            params["firstName"] = firstName
-            params["lastName"] = lastName
-        }
-        let request = RequestBuilder(url: url).parameters(params).build()
-        let resource = Resource<Jokes>(request: request, parseResponse: { data in
-            return try? JSONDecoder().decode(Jokes.self, from: data)
-        })
+        let resource = getRandomJokeResource(firstName: firstName, lastName: lastName, numberOfJokes: number)
         let jokes = try await networkService.load(resource: resource)
         return jokes.value.map { randomJokes in randomJokes.joke.htmlDecode() }
     }
 
     public func getJokeCategories(completion: @escaping ([String]) throws -> Void) {
-        let url = networkService.baseURL.appendingPathComponent("categories")
-        let request = RequestBuilder(url: url).build()
-        let resource = Resource<Category>(request: request) { data in
-            return try? JSONDecoder().decode(Category.self, from: data)
-        }
+        let resource = getCategoriesResource()
         let semaphore = DispatchSemaphore(value: 0)
         networkService.load(resource: resource) { result in
             defer {
@@ -91,12 +69,31 @@ public class MixInICNDBService: ICNDBService {
     }
 
     public func getJokeCategories() async throws -> [String] {
+        let resource = getCategoriesResource()
+        let category = try await networkService.load(resource: resource)
+        return category.names
+    }
+
+    private func getRandomJokeResource(firstName: String? = nil, lastName: String? = nil, numberOfJokes number: Int = 1) -> Resource<Jokes> {
+        let url = networkService.baseURL.appendingPathComponent("jokes/random/\(number)")
+        var params: [String: String] = [:]
+        if let firstName = firstName, let lastName = lastName {
+            params["firstName"] = firstName
+            params["lastName"] = lastName
+        }
+        let request = RequestBuilder(url: url).parameters(params).build()
+        let resource = Resource<Jokes>(request: request, parseResponse: { data in
+            return try? JSONDecoder().decode(Jokes.self, from: data)
+        })
+        return resource
+    }
+
+    private func getCategoriesResource() -> Resource<Category> {
         let url = networkService.baseURL.appendingPathComponent("categories")
         let request = RequestBuilder(url: url).build()
         let resource = Resource<Category>(request: request) { data in
             return try? JSONDecoder().decode(Category.self, from: data)
         }
-        let category = try await networkService.load(resource: resource)
-        return category.names
+        return resource
     }
 }
